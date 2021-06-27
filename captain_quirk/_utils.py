9e477ae59a94303d4a1a3f489ff450923008f5ea
Eileen Kuehn <eileen.kuehn@kit.edu>
2021-06-27 13:02:54 +0200

    abstract definition of a circuit representation

---
+++ captain_quirk/_utils.py
@@ -0,0 +1,32 @@
+from typing import List, Union
+from itertools import zip_longest
+
+
+EMPTY = 1
+
+
+class AbstractSyntaxGrid:
+    __slots__ = "_columns"
+
+    def __init__(self) -> None:
+        self._columns = []
+
+    def append(self, column: List[Union[int, str]]):
+        try:
+            self._merge(column)
+        except IndexError:
+            self._columns.append(column)
+    
+    def _merge(self, column: List[Union[int, str]]):
+        new_column = []
+        for old, new in zip_longest(self._columns[-1], column, fillvalue=EMPTY):
+            if old == EMPTY:
+                new_column.append(new)
+            elif new == EMPTY:
+                new_column.append(old)
+            else:
+                raise IndexError
+        self._columns[-1] = new_column
+
+    def to_json(self):
+        return self._columns.copy()
